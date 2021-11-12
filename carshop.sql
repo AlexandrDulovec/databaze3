9fa19247dcc7d0518b92c1e493765c06b189cb85
AlexandrDulovec <79139997+AlexandrDulovec@users.noreply.github.com>
2021-11-12 11:50:09 +0100

    ...

---
+++ carshop.sql
@@ -0,0 +1,42 @@
+CREATE TABLE accesory (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  manufacturer VARCHAR(255),
+  model VARCHAR(255)
+  ); 
+
+id INT UNSIGNED AUTO_INCREMENT,
+INSERT INTO accesory (id, name, description),
+;
+
+SELECT *
+FROM accesory
+
+INSERT INTO accesory (id, name, description)
+VALUES ('1', 'Adaptivní tempomat', 'adaptivnitepmomat');
+
+INSERT INTO accesory (id, name, description)
+VALUES ('2', 'Parkovací kamera', 'parkovacikamera');
+
+INSERT INTO accesory (id, name, description)
+VALUES ('3', 'Parkovací senzory', 'parkovacisenzory');
+
+INSERT INTO accesory (id, name, description)
+VALUES ('4', 'Vyhřívaná sedadla', 'vyhrivanasedadla');
+
+INSERT INTO accesory (id, name, description)
+VALUES ('5', 'Tempomat', 'tempomat');
+
+INSERT INTO accesory (id, name, description)
+VALUES ('6', 'Satelitní navigace', 'satelitninavigace');
+
+INSERT INTO accesory (id, name, description)
+VALUES ('7', 'Litá kola', 'litakola');
+
+INSERT INTO accesory (id, name, description)
+VALUES ('8', 'Tažné zařízení', 'taznezarizeni');
+
+INSERT INTO accesory (id, name, description)
+VALUES ('9', 'Automatické svícení', 'automatickeosviceni');
+
+INSERT INTO accesory (id, name, description)
+VALUES ('10', 'Kožená sedadla', 'kozenasedadla');
